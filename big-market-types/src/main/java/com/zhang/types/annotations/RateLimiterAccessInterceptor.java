6ed14e0e0773feffa52ea23e61abace7847309d4
张俊杰 <2298532487@qq.com>
2026-07-05 21:11:55 +0800

    feat:  营销服务 第30节：分布式动态限流和熔断

---
+++ big-market-types/src/main/java/com/zhang/types/annotations/RateLimiterAccessInterceptor.java
@@ -0,0 +1,35 @@
+package com.zhang.types.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author: ZhangJunjie
+ * @Description:
+ * @DateTime: 2026/7/5 9:34
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Documented
+public @interface RateLimiterAccessInterceptor {
+
+    /**
+     * 用哪个字段作为拦截标识，未配置则默认走全部
+     */
+    String key() default "all";
+
+    /**
+     * 限制频次（每秒请求次数）
+     */
+    double permitsPerSecond();
+
+    /**
+     * 黑名单拦截（多少次限制后加入黑名单）0 不限制
+     */
+    double blacklistCount() default 0;
+
+    /**
+     * 拦截后的执行方法
+     */
+    String fallbackMethod();
+
+}
